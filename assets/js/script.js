6bc41f4bd319e9409baec91b6a640398713159c3
chrisgustafsson <>
2023-10-14 19:45:10 +0000

    JS run game

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,13 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+/**
+ * The main game "loop"
+ * user's answer
+ */
 function runGame() {
-
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
